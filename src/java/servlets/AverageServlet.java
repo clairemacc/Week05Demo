d9ff3dc37608ed2f92e1a3b816fec31d6b71d735
822408 <822408@SAIT224224IT.ACDM.DS.SAIT.CA>
2020-10-11 14:11:25 -0600

    Finished Demo Added functionality and verification

--- src/java/servlets/AverageServlet.java
+++ src/java/servlets/AverageServlet.java
@@ -2,10 +2,12 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -16,6 +18,38 @@ public class AverageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        
+        String action = request.getParameter("action");
+        if (action != null && action.equals("reset")) {
+            session.invalidate();
+            session = request.getSession();
+        }
+        
+        ArrayList<Integer> numbers = (ArrayList<Integer>) session.getAttribute("numbers");
+        if (numbers == null)
+            numbers = new ArrayList<>();
+        
+        if (request.getParameter("number") != null) {
+            int number = Integer.parseInt(request.getParameter("number"));
+            numbers.add(number);
+            //update session
+            session.setAttribute("numbers", numbers);
+        }
+        
+        //calculate average
+        double average = 0.0;
+        
+        for (int n : numbers) 
+            average += n;
+        
+        if (numbers.size() > 0)
+            average /= numbers.size();
+        
+        //display average
+        request.setAttribute("average", average);
+        
         getServletContext().getRequestDispatcher("/WEB-INF/average.jsp")
                 .forward(request, response);
     }
